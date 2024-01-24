b0de19ec8ac9d1de4d595745954cdfcb394c7312
Philipp Wiedemann <ucjbh@student.kit.edu>
2024-01-24 23:48:06 +0100

    add vector e field, fix color but make it worse, add e field arrows :)

--- graphic_pygame.py
+++ graphic_pygame.py
@@ -17,23 +17,26 @@ green = (21, 176, 26)
 
 
 # Set up physics
-time_step = 0.1
+time_step = 0.01
 
-mouse_charge = Charge(-10)
-test_charge = Charge(1, init_position=np.array([300.,300.,0.]))
+
+Space = Field_Area(WIDTH, HEIGHT, (-10, 10), (-10, 10))
+
+
+mouse_charge = Space.add_charge(charge=-10)
+test_charge = Space.add_charge(charge=1)
 
 electric_field_surface = pygame.Surface((WIDTH, HEIGHT))
 electric_field_colors = np.ones((WIDTH, HEIGHT, 3), dtype=np.uint8)
 
-baum = Field_Area(WIDTH, HEIGHT, (-10, 10), (-10, 10))
 
 print(electric_field_colors)
 print(np.array([1,2,3]).shape)
 print((np.array([[1.2],[5]]) @ np.array([1,0,3,5]).reshape(1,4)).round())
 electric_field_colors[200:300,200:455] =  np.linspace(0,127,255).reshape(255,1) @ np.array([2,1,0]).reshape(1,3)
 
-baum.set_test_e_field(mouse_charge.charge, mouse_charge.position)
-electric_field_colors = baum.E_field_in_color(saturation_point=1)
+Space.set_test_e_field(mouse_charge.charge, (0,0,0))
+electric_field_colors = Space.E_field_in_color(saturation_point=1)
 
 
 # Main game loop
@@ -49,12 +52,12 @@ while running:
     mouse_x, mouse_y = pygame.mouse.get_pos()
 
     ### UPDATE PHYSICS ###
-    mouse_charge.set_update(time_step, np.array([mouse_x, mouse_y, 0]))
-    test_charge.update(time_step,baum.E,1)
+    mouse_charge.set_update(time_step, Space.position_at_index((mouse_x, mouse_y)))
+    test_charge.update(time_step)
 
-    
-    baum.set_test_e_field(mouse_charge.charge, mouse_charge.position)
-    electric_field_colors = baum.E_field_in_color(saturation_point=1)
+
+    Space.set_test_e_field(mouse_charge.charge, mouse_charge.position)
+    electric_field_colors = Space.E_field_in_color(saturation_point=5)
 
 
     pygame.surfarray.blit_array(electric_field_surface, electric_field_colors)
@@ -63,8 +66,18 @@ while running:
     screen.fill(black)
 
     screen.blit(electric_field_surface, (0,0))
-    pygame.draw.circle(screen, white, (mouse_x, mouse_y), 5)
-    pygame.draw.circle(screen, green, (test_charge.x(), test_charge.y()), 5)
+
+    pygame.draw.circle(screen, white, Space.index_of_position(mouse_charge.position), 5)
+    pygame.draw.circle(screen, green, Space.index_of_position(test_charge.position), 5)
+    pygame.draw.circle(screen, white, Space.index_of_position(np.zeros(3)), 4)
+    pygame.draw.circle(screen, white, Space.index_of_position(np.array([1,0,0])), 3)
+    pygame.draw.circle(screen, white, Space.index_of_position(np.array([0,1,0])), 3)
+
+
+    for pos in Space.position[200:500:10, 200:500:10].reshape(-1, 3):
+        start = Space.index_of_position(pos)
+        end = start + Space.E_at_position(pos)[:2]
+        pygame.draw.line(screen, white, start, end, width=1)
 
     # Update the display
     pygame.display.flip()
--- maxwell_calculation.py
+++ maxwell_calculation.py
@@ -7,30 +7,26 @@ RED = (255,0,0)
 def vectorfield_dot_product(vector_field, other_vector_field):
         return np.sum(vector_field * other_vector_field, axis=-1)
         
-def vectorfield_scalar_product(vector_field, scalar_field):
+def vectorfield_scalarfield_product(vector_field, scalar_field):
     return vector_field * scalar_field[:,:,np.newaxis]
 
+def vector_scalarfield_product(vector, scalar_field):
+    return np.tile(vector, (*scalar_field.shape, 1)) * scalar_field[:,:,np.newaxis]
+
 class Charge():
-    def __init__(self, charge=1, mass=1, init_position=np.zeros(3), init_velocity=np.zeros(3)) -> None:
+    def __init__(self, field: 'Field_Area', charge=1, mass=1, init_position=np.zeros(3), init_velocity=np.zeros(3)) -> None:
         self.charge = charge
         self.mass = mass
-        self.position = init_position
+        self.position = init_position # np.array([x,y,z])
         self.velocity = init_velocity
         self.acceleration = np.zeros(3)
+        self.field = field
 
-    def x(self):
-        return self.position[0]
-    def y(self):
-        return self.position[1]
-    def z(self):
-        return self.position[2]
-
-    def lorentz_force(self, E, B):
-        return self.charge * (E[int(self.x()), int(self.y()),:] + np.cross(self.velocity, np.array([0,0,0])))
+    def lorentz_force(self):
+        return self.charge * (self.field.E_at_position(self.position) )#+ np.cross(self.velocity, np.array([0,0,0])))
     
-
-    def update(self, dt, E, B):
-        self.acceleration = self.lorentz_force(E, B) / self.mass
+    def update(self, dt):
+        self.acceleration = self.lorentz_force() / self.mass
         self.position += self.velocity * dt
         self.velocity += self.acceleration * dt
 
@@ -40,9 +36,6 @@ class Charge():
         self.position = pos
         self.velocity = (self.position - old_pos) / dt
         self.acceleration = (self.velocity - old_vel) / dt
-
-
-    
 
 
 class Fieldwave():
@@ -69,6 +62,9 @@ class Field_Area():
         self.E = np.ones((x_resolution, y_resolution, 3))
         self.B = np.zeros((x_resolution, y_resolution, 3))
 
+        self.x_resolution = x_resolution
+        self.y_resolution = y_resolution
+
         self.x_min, self.x_max = x_range
         self.y_min, self.y_max = y_range
 
@@ -76,7 +72,6 @@ class Field_Area():
         self.y_width = self.y_max - self.y_min
 
         self.pixel_size = (self.x_width / (x_resolution - 1), self.y_width / (y_resolution - 1))
-        self.origin = (x_resolution/2, y_resolution/2)
 
         x = np.linspace(self.x_min, self.x_max, x_resolution)
         y = np.linspace(self.y_min, self.y_max, y_resolution)
@@ -84,23 +79,38 @@ class Field_Area():
         y_grid = np.tile(y, (x_resolution, 1)).T
         z_grid = np.zeros_like(x_grid)
 
-        self.position = np.stack((x_grid, y_grid, z_grid), axis=-1)
+        self.position = np.stack((y_grid, x_grid, z_grid), axis=-1)
 
-        self.charges = None
+        self.charges = []
+
+    def add_charge(self, **charge_properties):
+        new_charge = Charge(self, **charge_properties)
+        self.charges.append(new_charge)
+        return new_charge
 
 
     def position_at_index(self, index):
-        print(index)
         return self.position[index]
 
+    def index_of_position(self, position):
+        x_index = round((position[0] - self.x_min) / self.pixel_size[0])
+        y_index = round((position[1] - self.y_min) / self.pixel_size[1])
 
-    def set_test_e_field(self, charge, charge_idx):
-        charge_pos = self.position_at_index(charge_idx)
+        if x_index < 0 or x_index >= self.x_resolution:
+            print('x-Index out of bound, returning 0')
+            return (0,0)
+        if y_index < 0 or y_index >= self.y_resolution:
+            print('y-Index out of bound, returning 0')
+            return (0,0)
+        return x_index, y_index
+    
+    def E_at_position(self, position):
+        return self.E[self.index_of_position(position)]
+    
 
-
+    def set_test_e_field(self, charge, charge_pos):
         vec_to_q = self.position - charge_pos
-
-        self.E = charge * vectorfield_scalar_product(vec_to_q, 1 / np.linalg.norm(vec_to_q, axis=2))
+        self.E = charge * vectorfield_scalarfield_product(vec_to_q, 1 / np.linalg.norm(vec_to_q, axis=2)**2)
 
 
     def E_field_in_color(self, color_positive=RED, color_negative=BLUE, saturation_point=1, x_range='full', y_range='full'):
@@ -108,24 +118,26 @@ class Field_Area():
         E_field = self.E / saturation_point
         E_field_norm = np.linalg.norm(E_field, axis=2)
 
+        return vector_scalarfield_product(BLUE, E_field_norm).round()
+
         # print(np.where(E_field >= 1))
 
         # a = np.array([[1,0,0,1],[0,0,1,2]])
         # print((a>1) &( a<=0))
         # print([*a.shape, 15])
 
-        color_field[E_field_norm >= 1] = color_positive
-        color_field[E_field_norm <= -1] = color_negative
+        # color_field[E_field_norm >= 1] = color_positive
+        # color_field[E_field_norm <= -1] = color_negative
 
-        small_positive = (E_field_norm < 1) & (E_field_norm >=0)
-        small_E_positive = E_field_norm[np.where(small_positive)]
-        color_field[small_positive] = (small_E_positive.reshape(len(small_E_positive), 1) @ np.array(color_positive).reshape(1,3)).round()
+        # small_positive = (E_field_norm < 1) & (E_field_norm >=0)
+        # small_E_positive = E_field_norm[np.where(small_positive)]
+        # color_field[small_positive] = (small_E_positive.reshape(len(small_E_positive), 1) @ np.array(color_positive).reshape(1,3)).round()
         
-        small_negative = (E_field_norm > -1) & (E_field_norm < 0)
-        small_E_negative = np.abs(E_field_norm[np.where(small_negative)])
-        color_field[small_negative] = (small_E_negative.reshape(len(small_E_negative), 1) @ np.array(color_negative).reshape(1,3)).round()
+        # small_negative = (E_field_norm > -1) & (E_field_norm < 0)
+        # small_E_negative = np.abs(E_field_norm[np.where(small_negative)])
+        # color_field[small_negative] = (small_E_negative.reshape(len(small_E_negative), 1) @ np.array(color_negative).reshape(1,3)).round()
 
-        return color_field
+        # return color_field
     
 
 
@@ -140,9 +152,15 @@ class Field_Area():
 # f = np.array([1,2,3])
 
 # print(c)
-# print(d)
-# print()
+# print(c[1:4,2])
+# # print(d)
+# # print()
 # print(e)
+# print('rntrn')
+# print(e[1:5,1:4].reshape(-1, 3))
+# print('here')
+# print(vector_scalarfield_product(f, c))
+    
 
 # print('here')
 # print(vector_dot_product(e, e))
@@ -151,6 +169,6 @@ class Field_Area():
 
 baum = Field_Area(5, 5, (-10, 10), (-10, 10))
 
-baum.set_test_e_field(1, (2,3))
+baum.set_test_e_field(1, (0,0,0))
 baum.E_field_in_color()
 
